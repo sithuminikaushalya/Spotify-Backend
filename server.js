e62de2b1a2def37969e6c40efe6f4e5868eb218c
Sithumini Kaushalya <118107125+sithuminikaushalya@users.noreply.github.com>
2024-07-31 00:17:43 +0530

    adding initializing files

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
+
+// Connect to MongoDB
+connectDB();
 
 // app config
 const app = express();
@@ -11,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 // initializing routes
+app.use("/api/song", songRouter);
+
 app.get('/', (req, res) => res.send("API Working"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ src/controllers/songController.js
@@ -0,0 +1,11 @@
+
+
+const addSong = async (req,res) => {
+
+}
+
+const listSong = async (req,res) => {
+
+}
+
+export {addSong, listSong}
